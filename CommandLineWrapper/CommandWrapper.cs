e7e60e3bd49282bdaa1b14ade1de86451da34e17
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-23 18:36:40 +0100

    Update CommandWrapper.cs

--- CommandLineWrapper/CommandWrapper.cs
+++ CommandLineWrapper/CommandWrapper.cs
@@ -14,17 +14,19 @@ public sealed class CommandWrapper
     /// <summary>
     /// Description of the <see cref="Command"/>.
     /// </summary>
-    private readonly string? _description;
+    private string? _description;
+
+    private readonly IList<Command> _commands;
 
     /// <summary>
     /// Initializes a <see langword="new"/> instance of <see cref="CommandWrapper"/> with the given <paramref name="name"/> and <paramref name="description"/>.
     /// </summary>
     /// <param name="name">Name of the <see cref="Command"/>.</param>
     /// <param name="description">Description of the <see cref="Command"/>.</param>
-    private CommandWrapper(string name, string? description = null)
+    private CommandWrapper(string name)
     {
         _name = name;
-        _description = description;
+        _commands = new List<Command>();
     }
 
     /// <summary>
@@ -34,5 +36,15 @@ public sealed class CommandWrapper
     /// <param name="name">Name of the <see cref="Command"/>.</param>
     /// <returns>A <see langword="new"/> instance <see cref="CommandWrapper"/> with the given <paramref name="name"/>.</returns>
     public static CommandWrapper Create(string name)
-        => new(name, null);
+        => new(name);
+    /// <summary>
+    /// Sets the description for the <see cref="Command"/>.
+    /// </summary>
+    /// <param name="description">The description that will be set.</param>
+    /// <returns>The <see cref="CommandWrapper"/> with its description set.</returns>
+    public CommandWrapper SetDescription(string description)
+    {
+        _description = description;
+        return this;
+    }
 }
