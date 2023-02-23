66def60f5a3aa836b4cc55c3976c2fa0e2054b97
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-23 18:36:20 +0100

    Create Program.cs

---
+++ CommandLineWrapper.TestConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿using CommandLineWrapper;
+using System.CommandLine;
+
+RootCommand rootCommand = new("This is a test command.");
+
+rootCommand.AddCommand(CommandWrapper.Create("subcommand")
+    .SetDescription("This is a subcommand")
+    .ToCommand());
+
+await rootCommand.InvokeAsync(args);
