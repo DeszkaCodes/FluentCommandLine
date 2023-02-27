67a85eb3cb13f0bf8db26da3c17c2637fd8a6312
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-27 23:50:29 +0100

    Create Extensions.cs

---
+++ CommandLineWrapper.UnitTest/Extensions.cs
@@ -0,0 +1,7 @@
+﻿namespace CommandLineWrapper.UnitTest;
+
+internal static class Extensions
+{
+    public static bool HaveSameElements<T>(this IEnumerable<T> a, IEnumerable<T> b)
+        => a.All(b.Contains) && b.All(a.Contains);
+}
