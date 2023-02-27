6195b74e972a43da2f8d6728af8ee9cceed37a2c
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-27 23:09:36 +0100

    Create IdentifierSymbolWrapperBase.cs

---
+++ CommandLineWrapper/Abstraction/IdentifierSymbolWrapperBase.cs
@@ -0,0 +1,22 @@
+﻿using CommandLineWrapper.Abstraction.Interfaces;
+
+namespace CommandLineWrapper.Abstraction;
+
+/// <summary>
+/// Defines a base for an <see cref="System.CommandLine.IdentifierSymbol"/> wrapper.
+/// </summary>
+public abstract class IdentifierSymbolWrapperBase : SymbolWrapperBase,
+    IIdentifierSymbolCanSetName, IIdentifierSymbolCanSetProperties
+{
+    /// <inheritdoc cref="IIdentifierSymbolCanSetName.SetName(string)"/>
+    public override abstract IIdentifierSymbolCanSetProperties SetName(string name);
+
+    /// <inheritdoc cref="IIdentifierSymbolCanSetProperties.SetDescription(string)"/>
+    public override abstract IIdentifierSymbolCanSetProperties SetDescription(string description);
+
+    /// <inheritdoc cref="IIdentifierSymbolCanSetProperties.AddAlias(string)"/>
+    public abstract IIdentifierSymbolCanSetProperties AddAlias(string alias);
+
+    /// <inheritdoc cref="IIdentifierSymbolCanSetProperties.SetHidden(bool)"/>
+    public override abstract IIdentifierSymbolCanSetProperties SetHidden(bool isHidden);
+}
