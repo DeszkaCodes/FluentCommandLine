c4a7139183c938f708bc22c4ba53214349a227b5
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-27 23:09:32 +0100

    Create SymbolWrapperBase.cs

---
+++ CommandLineWrapper/Abstraction/SymbolWrapperBase.cs
@@ -0,0 +1,18 @@
+﻿using CommandLineWrapper.Abstraction.Interfaces;
+
+namespace CommandLineWrapper.Abstraction;
+
+/// <summary>
+/// Defines a base for a <see cref="System.CommandLine.Symbol"/> wrapper.
+/// </summary>
+public abstract class SymbolWrapperBase : ISymbolCanSetName, ISymbolCanSetProperties
+{
+    /// <inheritdoc />
+    public abstract ISymbolCanSetProperties SetName(string name);
+
+    /// <inheritdoc />
+    public abstract ISymbolCanSetProperties SetDescription(string description);
+
+    /// <inheritdoc />
+    public abstract ISymbolCanSetProperties SetHidden(bool isHidden);
+}
