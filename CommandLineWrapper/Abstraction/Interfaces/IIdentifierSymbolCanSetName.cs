afd55505671db8a3e19b88f42112537641ccb3fe
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-27 23:09:17 +0100

    Create IIdentifierSymbolCanSetName.cs

---
+++ CommandLineWrapper/Abstraction/Interfaces/IIdentifierSymbolCanSetName.cs
@@ -0,0 +1,11 @@
+﻿namespace CommandLineWrapper.Abstraction.Interfaces;
+
+/// <summary>
+/// Defines a fluent method responsible for setting the name of a <see cref="System.CommandLine.IdentifierSymbol"/>.
+/// </summary>
+public interface IIdentifierSymbolCanSetName : ISymbolCanSetName
+{
+    /// <returns>An <see cref="IIdentifierSymbolCanSetProperties"/> with the given name.</returns>
+    /// <inheritdoc cref="ISymbolCanSetName.SetName(string)"/>
+    new IIdentifierSymbolCanSetProperties SetName(string name);
+}
