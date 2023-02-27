ac6c200dfad72573570df9e47ad2ba7a35668868
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-27 23:08:45 +0100

    Create ISymbolCanSetProperties.cs

---
+++ CommandLineWrapper/Abstraction/Interfaces/ISymbolCanSetProperties.cs
@@ -0,0 +1,21 @@
+﻿namespace CommandLineWrapper.Abstraction.Interfaces;
+
+/// <summary>
+/// Defines fluent methods responsible for setting the properties of a <see cref="System.CommandLine.Symbol"/>.
+/// </summary>
+public interface ISymbolCanSetProperties
+{
+    /// <summary>
+    /// Sets a value indicating whether the symbol is hidden.
+    /// </summary>
+    /// <param name="isHidden">The new value indicating whether the symbol is hidden.</param>
+    /// <returns>An <see cref="ISymbolCanSetProperties"/> with the new value.</returns>
+    ISymbolCanSetProperties SetHidden(bool isHidden);
+
+    /// <summary>
+    /// Sets the description of the symbol.
+    /// </summary>
+    /// <param name="description">The new description of the symbol.</param>
+    /// <returns>An <see cref="ISymbolCanSetProperties"/> with the new description.</returns>
+    ISymbolCanSetProperties SetDescription(string description);
+}
