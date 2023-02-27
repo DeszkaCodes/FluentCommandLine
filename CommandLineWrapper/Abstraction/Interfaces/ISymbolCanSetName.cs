2d42d6128bcca20caf7ca0e24a2c14605c9f24a1
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-27 23:08:58 +0100

    Create ISymbolCanSetName.cs

---
+++ CommandLineWrapper/Abstraction/Interfaces/ISymbolCanSetName.cs
@@ -0,0 +1,14 @@
+﻿namespace CommandLineWrapper.Abstraction.Interfaces;
+
+/// <summary>
+/// Defines a fluent method responsible for setting the name of a <see cref="System.CommandLine.Symbol"/>.
+/// </summary>
+public interface ISymbolCanSetName
+{
+    /// <summary>
+    /// Sets the name of the symbol.
+    /// </summary>
+    /// <param name="name">The name of the symbol.</param>
+    /// <returns>An <see cref="ISymbolCanSetProperties"/> with the given name.</returns>
+    ISymbolCanSetProperties SetName(string name);
+}
