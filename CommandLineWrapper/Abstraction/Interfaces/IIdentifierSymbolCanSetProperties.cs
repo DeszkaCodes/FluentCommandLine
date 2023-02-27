fea246dae73593d33eaf1d14469d1768ab815b41
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-27 23:09:13 +0100

    Create IIdentifierSymbolCanSetProperties.cs

---
+++ CommandLineWrapper/Abstraction/Interfaces/IIdentifierSymbolCanSetProperties.cs
@@ -0,0 +1,22 @@
+﻿namespace CommandLineWrapper.Abstraction.Interfaces;
+
+/// <summary>
+/// Defines fluent methods responsible for setting the properties of a <see cref="System.CommandLine.IdentifierSymbol"/>.
+/// </summary>
+public interface IIdentifierSymbolCanSetProperties : ISymbolCanSetProperties
+{
+    /// <returns>An <see cref="IIdentifierSymbolCanSetProperties"/> with the new description.</returns>
+    /// <inheritdoc cref="ISymbolCanSetProperties.SetDescription(string)"/>
+    new IIdentifierSymbolCanSetProperties SetDescription(string description);
+
+    /// <returns>An <see cref="IIdentifierSymbolCanSetProperties"/> with the new value.</returns>
+    /// <inheritdoc cref="ISymbolCanSetProperties.SetHidden(bool)"/>
+    new IIdentifierSymbolCanSetProperties SetHidden(bool isHidden);
+
+    /// <summary>
+    /// Adds an alias. Multiple aliases can be added, most often used to provide a shorthand alternative.
+    /// </summary>
+    /// <param name="alias">The alias to add.</param>
+    /// <returns>An <see cref="IIdentifierSymbolCanSetProperties"/> with the added alias.</returns>
+    IIdentifierSymbolCanSetProperties AddAlias(string alias);
+}
