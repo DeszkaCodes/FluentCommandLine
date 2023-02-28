abd7598674f5b114408a52624136c75a446f3f1c
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-28 19:06:59 +0100

    Create RootCommandWrapper.cs

---
+++ CommandLineWrapper/RootCommandWrapper.cs
@@ -0,0 +1,85 @@
+﻿using CommandLineWrapper.Abstraction;
+using CommandLineWrapper.Abstraction.Interfaces;
+using System.CommandLine;
+
+namespace CommandLineWrapper;
+
+public sealed class RootCommandWrapper : CommandWrapperBase,
+    IRootCommandCanSetProperties
+{
+    private new readonly RootCommand _command;
+
+    private RootCommandWrapper()
+        : base(RootCommand.ExecutableName)
+    {
+        _command= new RootCommand(RootCommand.ExecutableName);
+    }
+
+    /// <summary>
+    /// Creates a new instance of <see cref="RootCommandWrapper"/>.
+    /// </summary>
+    /// <returns>The new instance as <see cref="IRootCommandCanSetProperties"/>.</returns>
+    public static IRootCommandCanSetProperties Create() => new RootCommandWrapper();
+
+    /// <inheritdoc />
+    public RootCommand GetRootCommand()
+    {
+        return _command;
+    }
+
+    /// <inheritdoc cref="IRootCommandCanSetProperties.AddAlias(string)"/>
+    public override IRootCommandCanSetProperties AddAlias(string alias)
+    {
+        base.AddAlias(alias);
+        return this;
+    }
+
+    /// <inheritdoc cref="IRootCommandCanSetProperties.AddArgument(Argument)"/>
+    public override IRootCommandCanSetProperties AddArgument(Argument argument)
+    {
+        base.AddArgument(argument);
+        return this;
+    }
+
+    /// <inheritdoc cref="IRootCommandCanSetProperties.AddCommand(Command)"/>
+    public override IRootCommandCanSetProperties AddCommand(Command command)
+    {
+        base.AddCommand(command);
+        return this;
+    }
+
+    /// <inheritdoc cref="IRootCommandCanSetProperties.AddGlobalOption(Option)"/>
+    public override IRootCommandCanSetProperties AddGlobalOption(Option option)
+    {
+        base.AddGlobalOption(option);
+        return this;
+    }
+
+    /// <inheritdoc cref="IRootCommandCanSetProperties.AddOption(Option)"/>
+    public override IRootCommandCanSetProperties AddOption(Option option)
+    {
+        base.AddOption(option);
+        return this;
+    }
+
+    /// <inheritdoc cref="IRootCommandCanSetProperties.DoTreatUnmatchedTokensAsErrors(bool)"/>
+    public override IRootCommandCanSetProperties DoTreatUnmatchedTokensAsErrors(bool treatUnmatchedTokensAsErrors)
+    {
+        base.DoTreatUnmatchedTokensAsErrors(treatUnmatchedTokensAsErrors);
+        return this;
+    }
+
+    /// <inheritdoc cref="IRootCommandCanSetProperties.SetDescription(string)"/>
+    public override IRootCommandCanSetProperties SetDescription(string description)
+    {
+        base.SetDescription(description);
+        return this;
+    }
+
+    /// <inheritdoc cref="IRootCommandCanSetProperties.SetHidden(bool)"/>
+    public override IRootCommandCanSetProperties SetHidden(bool isHidden)
+    {
+        base.SetHidden(isHidden);
+        return this;
+    }
+}
