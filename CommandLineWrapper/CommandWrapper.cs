087bc2c2ec98fd1f09081d227ac9570813a5c2e9
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-28 18:22:40 +0100

    Update CommandWrapper.cs

--- CommandLineWrapper/CommandWrapper.cs
+++ CommandLineWrapper/CommandWrapper.cs
@@ -16,58 +16,75 @@ public sealed class CommandWrapper : CommandWrapperBase,
         _command = new Command(DefaultCommandName);
     }
 
+    /// <summary>
+    /// Creates a new instance of <see cref="CommandWrapper"/>.
+    /// </summary>
+    /// <returns>The new instance as <see cref="ICommandCanSetName"/>.</returns>
     public static ICommandCanSetName Create() => new CommandWrapper();
 
-    public override Command ToCommand() => _command;
+    /// <inheritdoc />
+    public Command GetCommand()
+    {
+        return _command;
+    }
 
+    /// <inheritdoc />
     public override ICommandCanSetProperties AddAlias(string alias)
     {
         _command.AddAlias(alias);
         return this;
     }
 
+    /// <inheritdoc />
     public override ICommandCanSetProperties AddArgument(Argument argument)
     {
         _command.AddArgument(argument);
         return this;
     }
 
+    /// <inheritdoc />
     public override ICommandCanSetProperties AddCommand(Command command)
     {
         _command.AddCommand(command);
         return this;
     }
 
+    /// <inheritdoc />
     public override ICommandCanSetProperties AddGlobalOption(Option option)
     {
         _command.AddGlobalOption(option);
         return this;
     }
 
+    /// <inheritdoc />
     public override ICommandCanSetProperties AddOption(Option option)
     {
         _command.AddOption(option);
         return this;
     }
 
+    /// <inheritdoc />
     public override ICommandCanSetProperties DoTreatUnmatchedTokensAsErrors(bool treatUnmatchedTokensAsErrors)
     {
         _command.TreatUnmatchedTokensAsErrors = treatUnmatchedTokensAsErrors;
         return this;
     }
 
+    /// <inheritdoc />
     public override ICommandCanSetProperties SetHidden(bool isHidden)
     {
         _command.IsHidden = isHidden;
         return this;
     }
 
+    /// <inheritdoc />
     public override ICommandCanSetProperties SetName(string name)
     {
         _command.Name = name;
         return this;
     }
 
+    /// <inheritdoc />
     public override ICommandCanSetProperties SetDescription(string description)
     {
         _command.Description = description;
