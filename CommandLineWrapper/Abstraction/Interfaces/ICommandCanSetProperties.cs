716729d96f8ff834ba47918079db6dd9f0ed8261
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-28 18:21:18 +0100

    Update ICommandCanSetProperties.cs

--- CommandLineWrapper/Abstraction/Interfaces/ICommandCanSetProperties.cs
+++ CommandLineWrapper/Abstraction/Interfaces/ICommandCanSetProperties.cs
@@ -5,35 +5,35 @@ namespace CommandLineWrapper.Abstraction.Interfaces;
 /// <summary>
 /// Defines fluent methods responsible for setting the properties of a <see cref="Command"/>.
 /// </summary>
-public partial interface ICommandCanSetProperties : IIdentifierSymbolCanSetProperties
+public partial interface ICommandCanSetProperties : ICommandBaseCanSetProperties
 {
     /// <returns>An <see cref="ICommandCanSetProperties"/> with the new <paramref name="description"/>.</returns>
-    /// <inheritdoc cref="IIdentifierSymbolCanSetProperties.SetDescription(string)"/>
+    /// <inheritdoc cref="ICommandBaseCanSetProperties.SetDescription(string)"/>
     new ICommandCanSetProperties SetDescription(string description);
 
     /// <returns>An <see cref="ICommandCanSetProperties"/> with the new value.</returns>
-    /// <inheritdoc cref="IIdentifierSymbolCanSetProperties.SetHidden(bool)"/>
+    /// <inheritdoc cref="ICommandBaseCanSetProperties.SetHidden(bool)"/>
     new ICommandCanSetProperties SetHidden(bool isHidden);
 
     /// <returns>An <see cref="ICommandCanSetProperties"/> with the added <paramref name="alias"/>.</returns>
-    /// <inheritdoc cref="IIdentifierSymbolCanSetProperties.AddAlias(string)"/>
+    /// <inheritdoc cref="ICommandBaseCanSetProperties.AddAlias(string)"/>
     new ICommandCanSetProperties AddAlias(string alias);
 
     /// <returns>An <see cref="ICommandCanSetProperties"/> with the added <paramref name="argument"/>.</returns>
     /// <inheritdoc cref="Command.AddArgument(Argument)"/>
-    ICommandCanSetProperties AddArgument(Argument argument);
+    new ICommandCanSetProperties AddArgument(Argument argument);
 
     /// <returns>An <see cref="ICommandCanSetProperties"/> with the added <paramref name="option"/>.</returns>
     /// <inheritdoc cref="Command.AddOption(Option)"/>
-    ICommandCanSetProperties AddOption(Option option);
+    new ICommandCanSetProperties AddOption(Option option);
 
     /// <returns>An <see cref="ICommandCanSetProperties"/> with the added global <paramref name="option"/>.</returns>
     /// <inheritdoc cref="Command.AddGlobalOption(Option)"/>
-    ICommandCanSetProperties AddGlobalOption(Option option);
+    new ICommandCanSetProperties AddGlobalOption(Option option);
 
     /// <returns>An <see cref="ICommandCanSetProperties"/> with the added <paramref name="command"/>.</returns>
     /// <inheritdoc cref="Command.AddCommand(Command)"/>
-    ICommandCanSetProperties AddCommand(Command command);
+    new ICommandCanSetProperties AddCommand(Command command);
 
     /// <summary>
     /// Sets a value that indicates whether unmatched tokens should be treated as errors. For example,
@@ -41,11 +41,11 @@ public partial interface ICommandCanSetProperties : IIdentifierSymbolCanSetPrope
     /// </summary>
     /// <param name="treatUnmatchedTokensAsErrors">The value that indicates whether unmatched tokens should be treated as errors</param>
     /// <returns>An <see cref="ICommandCanSetProperties"/> with the new value.</returns>
-    ICommandCanSetProperties DoTreatUnmatchedTokensAsErrors(bool treatUnmatchedTokensAsErrors);
+    new ICommandCanSetProperties DoTreatUnmatchedTokensAsErrors(bool treatUnmatchedTokensAsErrors);
 
     /// <summary>
-    /// Returns the <see cref="Command"/> created by the wrapper.
+    /// Gets the <see cref="Command"/> created by the wrapper.
     /// </summary>
     /// <returns>The <see cref="Command"/> created by the wrapper.</returns>
-    Command ToCommand();
+    Command GetCommand();
 }
