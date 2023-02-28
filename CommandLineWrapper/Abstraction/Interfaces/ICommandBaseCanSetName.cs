78bdfedc753fe11c03ec8452b7d6329fb36c8abf
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-28 18:20:43 +0100

    Create ICommandBaseCanSetName.cs

---
+++ CommandLineWrapper/Abstraction/Interfaces/ICommandBaseCanSetName.cs
@@ -0,0 +1,11 @@
+﻿namespace CommandLineWrapper.Abstraction.Interfaces;
+
+/// <summary>
+/// Defines a fluent method responsible for setting the name of a <see cref="System.CommandLine.Command"/>.
+/// </summary>
+public interface ICommandBaseCanSetName : IIdentifierSymbolCanSetName
+{
+    /// <returns>An <see cref="ICommandBaseCanSetProperties"/> with the given name.</returns>
+    /// <inheritdoc cref="IIdentifierSymbolCanSetName.SetName(string)"/>
+    new ICommandBaseCanSetProperties SetName(string name);
+}
