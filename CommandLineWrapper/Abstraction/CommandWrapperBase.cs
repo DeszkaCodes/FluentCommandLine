b78a6ac8a92b88c06a0dfb12dc6a6ce5f8055f98
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-28 18:21:43 +0100

    Update CommandWrapperBase.cs

--- CommandLineWrapper/Abstraction/CommandWrapperBase.cs
+++ CommandLineWrapper/Abstraction/CommandWrapperBase.cs
@@ -7,35 +7,32 @@ namespace CommandLineWrapper.Abstraction;
 /// Defines a base for a <see cref="Command"/> wrapper.
 /// </summary>
 public abstract class CommandWrapperBase : IdentifierSymbolWrapperBase,
-    ICommandCanSetName, ICommandCanSetProperties
+    ICommandBaseCanSetName, ICommandBaseCanSetProperties
 {
-    /// <inheritdoc cref="ICommandCanSetName.SetName(string)"/>
-    public override abstract ICommandCanSetProperties SetName(string name);
+    /// <inheritdoc cref="ICommandBaseCanSetName.SetName(string)"/>
+    public override abstract ICommandBaseCanSetProperties SetName(string name);
 
-    /// <inheritdoc cref="ICommandCanSetProperties.SetDescription(string)"/>
-    public override abstract ICommandCanSetProperties SetDescription(string description);
+    /// <inheritdoc cref="ICommandBaseCanSetProperties.SetDescription(string)"/>
+    public override abstract ICommandBaseCanSetProperties SetDescription(string description);
 
-    /// <inheritdoc cref="ICommandCanSetProperties.AddAlias(string)"/>
-    public override abstract ICommandCanSetProperties AddAlias(string alias);
+    /// <inheritdoc cref="ICommandBaseCanSetProperties.AddAlias(string)"/>
+    public override abstract ICommandBaseCanSetProperties AddAlias(string alias);
 
-    /// <inheritdoc cref="ICommandCanSetProperties.SetHidden(bool)"/>
-    public override abstract ICommandCanSetProperties SetHidden(bool isHidden);
+    /// <inheritdoc cref="ICommandBaseCanSetProperties.SetHidden(bool)"/>
+    public override abstract ICommandBaseCanSetProperties SetHidden(bool isHidden);
 
-    /// <inheritdoc cref="ICommandCanSetProperties.AddArgument(Argument)"/>
-    public abstract ICommandCanSetProperties AddArgument(Argument argument);
+    /// <inheritdoc cref="ICommandBaseCanSetProperties.AddArgument(Argument)"/>
+    public abstract ICommandBaseCanSetProperties AddArgument(Argument argument);
 
-    /// <inheritdoc cref="ICommandCanSetProperties.AddOption(Option)"/>
-    public abstract ICommandCanSetProperties AddOption(Option option);
+    /// <inheritdoc cref="ICommandBaseCanSetProperties.AddOption(Option)"/>
+    public abstract ICommandBaseCanSetProperties AddOption(Option option);
 
-    /// <inheritdoc cref="ICommandCanSetProperties.AddGlobalOption(Option)"/>
-    public abstract ICommandCanSetProperties AddGlobalOption(Option option);
+    /// <inheritdoc cref="ICommandBaseCanSetProperties.AddGlobalOption(Option)"/>
+    public abstract ICommandBaseCanSetProperties AddGlobalOption(Option option);
 
-    /// <inheritdoc cref="ICommandCanSetProperties.AddCommand(Command)"/>
-    public abstract ICommandCanSetProperties AddCommand(Command subcommand);
+    /// <inheritdoc cref="ICommandBaseCanSetProperties.AddCommand(Command)"/>
+    public abstract ICommandBaseCanSetProperties AddCommand(Command subcommand);
 
-    /// <inheritdoc cref="ICommandCanSetProperties.DoTreatUnmatchedTokensAsErrors(bool)"/>
-    public abstract ICommandCanSetProperties DoTreatUnmatchedTokensAsErrors(bool treatUnmatchedTokensAsErrors);
-
-    /// <inheritdoc cref="ICommandCanSetProperties.ToCommand"/>
-    public abstract Command ToCommand();
+    /// <inheritdoc cref="ICommandBaseCanSetProperties.DoTreatUnmatchedTokensAsErrors(bool)"/>
+    public abstract ICommandBaseCanSetProperties DoTreatUnmatchedTokensAsErrors(bool treatUnmatchedTokensAsErrors);
 }
