0015e07f28189fc2ff556cad7cd5b9dcabce4b4e
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-01 00:26:53 +0100

    Update CommandWrapperBase.Handler.Func.cs

--- CommandLineWrapper/Abstraction/CommandWrapperBase/CommandWrapperBase.Handler.Func.cs
+++ CommandLineWrapper/Abstraction/CommandWrapperBase/CommandWrapperBase.Handler.Func.cs
@@ -12,62 +12,107 @@ public abstract partial class CommandWrapperBase<T> : IdentifierSymbolWrapperBas
     ICommandBaseCanSetName, ICommandBaseCanSetProperties
 {
     /// <inheritdoc/>
-    public ICommandBaseCanSetProperties SetHandler(Func<Task> handle)
+    public virtual ICommandBaseCanSetProperties SetHandler(Func<Task> handle)
     {
-        throw new NotImplementedException();
+        _command.SetHandler(handle);
+        return this;
     }
 
     /// <inheritdoc/>
-    public ICommandBaseCanSetProperties SetHandler(Func<InvocationContext, Task> handle)
+    public virtual ICommandBaseCanSetProperties SetHandler(Func<InvocationContext, Task> handle)
     {
-        throw new NotImplementedException();
+        _command.SetHandler(handle);
+        return this;
     }
 
     /// <inheritdoc/>
-    public ICommandBaseCanSetProperties SetHandler<T1>(Func<T1, Task> handle, IValueDescriptor<T1> symbol)
+    public virtual ICommandBaseCanSetProperties SetHandler<T>(Func<T, Task> handle, IValueDescriptor<T> symbol)
     {
-        throw new NotImplementedException();
+        _command.SetHandler(handle, symbol);
+        return this;
     }
 
     /// <inheritdoc/>
-    public ICommandBaseCanSetProperties SetHandler<T1, T2>(Func<T1, T2, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2)
+    public virtual ICommandBaseCanSetProperties SetHandler<T1, T2>(Func<T1, T2, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2)
     {
-        throw new NotImplementedException();
+        _command.SetHandler(handle,
+            symbol1,
+            symbol2);
+        return this;
     }
 
     /// <inheritdoc/>
-    public ICommandBaseCanSetProperties SetHandler<T1, T2, T3>(Func<T1, T2, T3, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2, IValueDescriptor<T3> symbol3)
+    public virtual ICommandBaseCanSetProperties SetHandler<T1, T2, T3>(Func<T1, T2, T3, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2, IValueDescriptor<T3> symbol3)
     {
-        throw new NotImplementedException();
+        _command.SetHandler(handle,
+            symbol1,
+            symbol2,
+            symbol3);
+        return this;
     }
 
     /// <inheritdoc/>
-    public ICommandBaseCanSetProperties SetHandler<T1, T2, T3, T4>(Func<T1, T2, T3, T4, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2, IValueDescriptor<T3> symbol3, IValueDescriptor<T4> symbol4)
+    public virtual ICommandBaseCanSetProperties SetHandler<T1, T2, T3, T4>(Func<T1, T2, T3, T4, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2, IValueDescriptor<T3> symbol3, IValueDescriptor<T4> symbol4)
     {
-        throw new NotImplementedException();
+        _command.SetHandler(handle,
+            symbol1,
+            symbol2,
+            symbol3,
+            symbol4);
+        return this;
     }
 
     /// <inheritdoc/>
-    public ICommandBaseCanSetProperties SetHandler<T1, T2, T3, T4, T5>(Func<T1, T2, T3, T4, T5, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2, IValueDescriptor<T3> symbol3, IValueDescriptor<T4> symbol4, IValueDescriptor<T5> symbol5)
+    public virtual ICommandBaseCanSetProperties SetHandler<T1, T2, T3, T4, T5>(Func<T1, T2, T3, T4, T5, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2, IValueDescriptor<T3> symbol3, IValueDescriptor<T4> symbol4, IValueDescriptor<T5> symbol5)
     {
-        throw new NotImplementedException();
+        _command.SetHandler(handle,
+            symbol1,
+            symbol2,
+            symbol3,
+            symbol4,
+            symbol5);
+        return this;
     }
 
     /// <inheritdoc/>
-    public ICommandBaseCanSetProperties SetHandler<T1, T2, T3, T4, T5, T6>(Func<T1, T2, T3, T4, T5, T6, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2, IValueDescriptor<T3> symbol3, IValueDescriptor<T4> symbol4, IValueDescriptor<T5> symbol5, IValueDescriptor<T6> symbol6)
+    public virtual ICommandBaseCanSetProperties SetHandler<T1, T2, T3, T4, T5, T6>(Func<T1, T2, T3, T4, T5, T6, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2, IValueDescriptor<T3> symbol3, IValueDescriptor<T4> symbol4, IValueDescriptor<T5> symbol5, IValueDescriptor<T6> symbol6)
     {
-        throw new NotImplementedException();
+        _command.SetHandler(handle,
+            symbol1,
+            symbol2,
+            symbol3,
+            symbol4,
+            symbol5,
+            symbol6);
+        return this;
     }
 
     /// <inheritdoc/>
-    public ICommandBaseCanSetProperties SetHandler<T1, T2, T3, T4, T5, T6, T7>(Func<T1, T2, T3, T4, T5, T6, T7, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2, IValueDescriptor<T3> symbol3, IValueDescriptor<T4> symbol4, IValueDescriptor<T5> symbol5, IValueDescriptor<T6> symbol6, IValueDescriptor<T7> symbol7)
+    public virtual ICommandBaseCanSetProperties SetHandler<T1, T2, T3, T4, T5, T6, T7>(Func<T1, T2, T3, T4, T5, T6, T7, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2, IValueDescriptor<T3> symbol3, IValueDescriptor<T4> symbol4, IValueDescriptor<T5> symbol5, IValueDescriptor<T6> symbol6, IValueDescriptor<T7> symbol7)
     {
-        throw new NotImplementedException();
+        _command.SetHandler(handle,
+            symbol1,
+            symbol2,
+            symbol3,
+            symbol4,
+            symbol5,
+            symbol6,
+            symbol7);
+        return this;
     }
 
     /// <inheritdoc/>
-    public ICommandBaseCanSetProperties SetHandler<T1, T2, T3, T4, T5, T6, T7, T8>(Func<T1, T2, T3, T4, T5, T6, T7, T8, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2, IValueDescriptor<T3> symbol3, IValueDescriptor<T4> symbol4, IValueDescriptor<T5> symbol5, IValueDescriptor<T6> symbol6, IValueDescriptor<T7> symbol7, IValueDescriptor<T8> symbol8)
+    public virtual ICommandBaseCanSetProperties SetHandler<T1, T2, T3, T4, T5, T6, T7, T8>(Func<T1, T2, T3, T4, T5, T6, T7, T8, Task> handle, IValueDescriptor<T1> symbol1, IValueDescriptor<T2> symbol2, IValueDescriptor<T3> symbol3, IValueDescriptor<T4> symbol4, IValueDescriptor<T5> symbol5, IValueDescriptor<T6> symbol6, IValueDescriptor<T7> symbol7, IValueDescriptor<T8> symbol8)
     {
-        throw new NotImplementedException();
+        _command.SetHandler(handle,
+            symbol1,
+            symbol2,
+            symbol3,
+            symbol4,
+            symbol5,
+            symbol6,
+            symbol7,
+            symbol8);
+        return this;
     }
 }
