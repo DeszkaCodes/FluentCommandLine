03aa5c50e2638587a40625c24aacc851d814ca34
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-01 00:38:53 +0100

    Update Program.cs

--- CommandLineWrapper.TestConsole/Program.cs
+++ CommandLineWrapper.TestConsole/Program.cs
@@ -5,12 +5,14 @@ Command subcommand = CommandWrapper.Create()
     .SetName("subtest")
     .AddAlias("subcommandtest")
     .SetDescription("This is a subcommand test.")
+    .SetHandler(() => Console.WriteLine("Subcommand was called."))
     .GetCommand();
 
 RootCommand rootCommand = RootCommandWrapper.Create()
     .SetDescription("This is a test command.")
     .AddAlias("tc")
     .AddCommand(subcommand)
+    .SetHandler(() => Console.WriteLine("Root was called."))
     .GetRootCommand();
 
 await rootCommand.InvokeAsync(args);
