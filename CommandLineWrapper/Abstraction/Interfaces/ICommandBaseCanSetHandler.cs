267c05d4d78e9d4cd3db441c2b7667b82c6e4c72
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-02-28 22:33:41 +0100

    Create ICommandBaseCanSetHandler.cs

---
+++ CommandLineWrapper/Abstraction/Interfaces/ICommandBaseCanSetHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace CommandLineWrapper.Abstraction.Interfaces;
+
+/// <summary>
+/// Defines fluent methods responsible for setting a handler of a <see cref="System.CommandLine.Command"/>.
+/// </summary>
+public partial interface ICommandBaseCanSetHandler
+{
+    /// <summary>
+    /// Sets the command's handler to <see langword="null"/>.
+    /// </summary>
+    /// <returns>An <see cref="ICommandBaseCanSetProperties"/> without a handler.</returns>
+    ICommandBaseCanSetProperties NoHandler();
+}
