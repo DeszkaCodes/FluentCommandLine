06c38a46b292a1c33f820040b7ed713f79d23e43
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-03 12:47:42 +0100

    Update Program.cs

--- FluentCommandLine.TestConsole/Program.cs
+++ FluentCommandLine.TestConsole/Program.cs
@@ -1,14 +1,14 @@
 ﻿using FluentCommandLine;
 using System.CommandLine;
 
-Command subcommand = CommandWrapper.Create()
+Command subcommand = FluentCommand.Create()
     .SetName("subtest")
     .AddAlias("subcommandtest")
     .SetDescription("This is a subcommand test.")
     .SetHandler(() => Console.WriteLine("Subcommand was called."))
     .GetCommand();
 
-RootCommand rootCommand = RootCommandWrapper.Create()
+RootCommand rootCommand = FluentRootCommand.Create()
     .SetDescription("This is a test command.")
     .AddAlias("tc")
     .AddCommand(subcommand)
