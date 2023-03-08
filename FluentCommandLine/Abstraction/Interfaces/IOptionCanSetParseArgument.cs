3aa1a620302c45cde1d0e5470b9148b25b5bcac0
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-08 16:07:03 +0100

    Create IOptionCanSetParseArgument.cs

---
+++ FluentCommandLine/Abstraction/Interfaces/IOptionCanSetParseArgument.cs
@@ -0,0 +1,16 @@
+﻿namespace FluentCommandLine.Abstraction.Interfaces;
+
+/// <summary>
+/// Defines fluent methods for setting the <see cref="IOptionCanSetParseArgument{T}"/>
+/// properties of an <see cref="System.CommandLine.Option{T}"/>
+/// </summary>
+/// <typeparam name="T">The <see cref="System.Type"/> that the option's arguments are expected to be parsed as.</typeparam>
+public interface IOptionCanSetParseArgument<T>
+{
+    /// <summary>
+    /// Sets a value that indicates whether the argument is default or not.
+    /// </summary>
+    /// <param name="isDefault">The value that will be set.</param>
+    /// <returns>An <see cref="IOptionComplete{T}"/> with it's value set.</returns>
+    IOptionHasToSetName<T> SetDefault(bool isDefault);
+}
