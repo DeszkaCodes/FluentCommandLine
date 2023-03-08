fa6852783005a010106922c21cb740b4a08184c5
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-08 16:06:58 +0100

    Create IOptionHasToSetName.cs

---
+++ FluentCommandLine/Abstraction/Interfaces/IOptionHasToSetName.cs
@@ -0,0 +1,19 @@
+﻿namespace FluentCommandLine.Abstraction.Interfaces;
+
+/// <summary>
+/// Defines fluent methods for setting the name of an <see cref="System.CommandLine.Option{T}"/>
+/// </summary>
+/// <typeparam name="T">The <see cref="System.Type"/> that the option's arguments are expected to be parsed as.</typeparam>
+public interface IOptionHasToSetName<T> : IIdentifierSymbolCanSetName
+{
+    /// <returns>An <see cref="IOptionCanSetProperties{T}"/> with the given name.</returns>
+    /// <inheritdoc cref="IIdentifierSymbolCanSetName.SetName(string)"/>
+    new IOptionCanSetProperties<T> SetName(string name);
+
+    /// <summary>
+    /// Sets the aliases of the identifier symbol.
+    /// </summary>
+    /// <param name="aliases">The aliases of the identifier symbol.</param>
+    /// <returns>An <see cref="IOptionCanSetProperties{T}"/> with the given aliases.</returns>
+    IOptionCanSetProperties<T> SetAliases(string[] aliases);
+}
