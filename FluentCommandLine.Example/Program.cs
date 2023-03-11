24c374c678e96a34fc74206757579ac78f8635ab
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-11 12:05:07 +0100

    Update Program.cs

--- FluentCommandLine.Example/Program.cs
+++ FluentCommandLine.Example/Program.cs
@@ -4,11 +4,30 @@
 
 using FluentCommandLine;
 using System.CommandLine;
+using System.CommandLine.Invocation;
+
+Option<bool> showLicenseOption = FluentOption<bool>.Create()
+    .HasNoArgument()
+    .SetAliases(new string[] { "--license", "-l" })
+    .SetDescription("Shows license")
+    .GetOption();
+
+Action<bool> aboutCommandHandler = (licenseOptionValue) =>
+{
+    if (licenseOptionValue)
+    {
+        Console.WriteLine("MIT");
+        return;
+    }
+
+    Console.WriteLine("A package.");
+};
 
 Command aboutCommand = FluentCommand.Create()
-    .SetHandler(() => Console.WriteLine("Template text."))
+    .SetHandler(aboutCommandHandler, showLicenseOption)
     .SetName("about")
     .SetDescription("Sums up the package and its uses.")
+    .AddOption(showLicenseOption)
     .GetCommand();
 
 RootCommand rootCommand = FluentRootCommand.Create()
