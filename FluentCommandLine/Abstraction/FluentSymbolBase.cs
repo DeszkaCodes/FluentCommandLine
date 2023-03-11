937fda33df57abbb0cdd145634294d034a2a8944
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-11 09:38:51 +0100

    Update added generics to FluentSymbolBase

--- FluentCommandLine/Abstraction/FluentSymbolBase.cs
+++ FluentCommandLine/Abstraction/FluentSymbolBase.cs
@@ -1,18 +1,43 @@
 ﻿using FluentCommandLine.Abstraction.Interfaces;
+using System.CommandLine;
 
 namespace FluentCommandLine.Abstraction;
 
 /// <summary>
-/// Defines a base for a <see cref="System.CommandLine.Symbol"/> wrapper.
+/// Defines a base for a <see cref="Symbol"/> wrapper.
 /// </summary>
-public abstract class FluentSymbolBase : ISymbolCanSetName, ISymbolCanSetProperties
+/// <typeparam name="T">The <see cref="Type"/> that inherits <see cref="Symbol"/>.</typeparam>
+public abstract class FluentSymbolBase<T> : ISymbolCanSetName, ISymbolCanSetProperties
+    where T : Symbol
 {
-    /// <inheritdoc />
-    public abstract ISymbolCanSetProperties SetName(string name);
+    /// <summary>
+    /// The generic type that is being wrapped.
+    /// </summary>
+    internal protected readonly T _wrapped;
+
+    private protected FluentSymbolBase(T wrapped)
+    {
+        _wrapped = wrapped;
+    }
 
     /// <inheritdoc />
-    public abstract ISymbolCanSetProperties SetDescription(string description);
+    public virtual ISymbolCanSetProperties SetName(string name)
+    {
+        _wrapped.Name = name;
+        return this;
+    }
 
     /// <inheritdoc />
-    public abstract ISymbolCanSetProperties SetHidden(bool isHidden);
+    public virtual ISymbolCanSetProperties SetDescription(string description)
+    {
+        _wrapped.Description = description;
+        return this;
+    }
+
+    /// <inheritdoc />
+    public virtual ISymbolCanSetProperties SetHidden(bool isHidden)
+    {
+        _wrapped.IsHidden = isHidden;
+        return this;
+    }
 }
