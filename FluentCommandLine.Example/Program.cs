96f6627255a218de453546a2097d647fbb4bfa4e
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-11 11:11:22 +0100

    Rename FluentCommandLine.TestConsole

---
+++ FluentCommandLine.Example/Program.cs
@@ -0,0 +1,20 @@
+﻿//using FluentCommandLine;
+//using System.CommandLine;
+//using System.Resources;
+
+using FluentCommandLine;
+using System.CommandLine;
+
+Command aboutCommand = FluentCommand.Create()
+    .SetName("about")
+    .SetDescription("Sums up the package and its uses.")
+    .SetHandler(() => Console.WriteLine("Template text."))
+    .GetCommand();
+
+RootCommand rootCommand = FluentRootCommand.Create()
+    .SetDescription("Showcase of the FluentCommandLine package")
+    .SetHandlerInvokeSelfHelp()
+    .AddCommand(aboutCommand)
+    .GetRootCommand();
+
+await rootCommand.InvokeAsync(args);
